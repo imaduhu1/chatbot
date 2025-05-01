a0e44416c25dca2a29ac091741140ad9add1c164
imaduhu1 <159795567+imaduhu1@users.noreply.github.com>
2025-05-01 10:49:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 
 # Load API key
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("https://github.com/imaduhu1/chatbot/blob/main/streamlit_app.py")
 
 # Instantiate v1 client
 client = OpenAI(api_key=api_key)
